120f648273359c343af66f8279dd7203309882c5
Basile Koko <basile_koko@hotmail.com>
2016-12-14 12:55:22 +0000

    adding info

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,26 +1,28 @@
+#Pair programming partner Sam Jone
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when pass 3' do
+  it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  it 'return "buzz" when pass 5' do
+  it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-  it 'return "fizzbuzz" when pass 15' do
+  it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 
-  it 'return fizz when pass multiple of 3' do
+  it 'returns fizz when passed multiple of 3' do
     expect(fizzbuzz(9)).to eq 'fizz'
   end
-  it 'return buzz when pass multiple of 5' do
+  it 'returns buzz when passed multiple of 5' do
     expect(fizzbuzz(20)).to eq 'buzz'
   end
-  it 'return "fizzbuzz" when pass multiple of 3 and 5' do
+  it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
     expect(fizzbuzz(30)).to eq 'fizzbuzz'
   end
-  it 'return num when pass a non multiple of 3 and 5' do
+  it 'return num  when passed a non multiple of 3 and 5' do
     expect(fizzbuzz(2)).to eq 2
   end
 end
+
